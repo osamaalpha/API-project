2d761402dd0cb8eb78d65c677d75c87c4ec47d8e
osamaalpha <osamamohamedupisdown@gmail.com>
2021-06-19 15:37:37 +0300

    make a src dir and update readme file

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # API-project
-Football week catch up app
+
+Football week catchup app using JavaScript
+
+ <img src="./footballimg.JPG">
+
+# Description
+
+<h2>Single page application making use of scorebat API showing all matches played last week in all football competitions with highlights </h2>
 
 ## Using ScoreBat API <a href="https://www.scorebat.com/video-api/">check it out</a>
 
---
+++ src/searchByTeam.js
@@ -0,0 +1,30 @@
+import { settingMatch } from "./settingMatch.js";
+import { reset } from "./reset.js";
+
+export function searchByTeam(getData, submitTeam, searchTeamError) {
+  const team = document.querySelector(".input-team");
+  submitTeam.addEventListener("click", () => {
+    searchTeamError.style.display = "none";
+    if (team.value === "" || team.value === " ") {
+      searchTeamError.style.display = "block";
+    }
+    reset();
+    getData.forEach((el) => {
+      if (
+        team.value.trim().toUpperCase() !== el.side1.name.toUpperCase() &&
+        team.value.trim().toUpperCase() !== el.side2.name.toUpperCase()
+      ) {
+        searchTeamError.style.display = "block";
+      }
+    });
+    getData.forEach((el) => {
+      if (
+        team.value.trim().toUpperCase() === el.side1.name.toUpperCase() ||
+        team.value.trim().toUpperCase() === el.side2.name.toUpperCase()
+      ) {
+        searchTeamError.style.display = "none";
+        settingMatch(el, submitTeam);
+      }
+    });
+  });
+}
